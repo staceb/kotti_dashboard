70dc81ca1da7aca733faf25d18f2a6c2db5608ea
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-10-28 11:43:53 -0500

    added new view for dashboard

--- kotti_dashboard/views/view.py
+++ kotti_dashboard/views/view.py
@@ -9,41 +9,18 @@ from pyramid.view import view_config
 from pyramid.view import view_defaults
 
 from kotti_dashboard import _
-from kotti_dashboard.resources import CustomContent
 from kotti_dashboard.fanstatic import css_and_js
 from kotti_dashboard.views import BaseView
 
+from kotti_compass.fanstatic import mystyles, font_awesome
 
-@view_defaults(context=CustomContent, permission='view')
-class CustomContentViews(BaseView):
-    """ Views for :class:`kotti_dashboard.resources.CustomContent` """
-
-    @view_config(name='view', permission='view',
-                 renderer='kotti_dashboard:templates/custom-content-default.pt')
+@view_defaults(permission="view")
+class DashboardView(BaseView):
+    @view_config(name='dashboard', permission='view',
+                 renderer='kotti_dashboard:templates/mainview.mako')
     def default_view(self):
-        """ Default view for :class:`kotti_dashboard.resources.CustomContent`
+        font_awesome.need()
+        mystyles['bootstrap-custom']['BlanchedAlmond'].need()
+        return dict()
+    
 
-        :result: Dictionary needed to render the template.
-        :rtype: dict
-        """
-
-        return {
-            'foo': _(u'bar'),
-        }
-
-    @view_config(name='alternative-view', permission='view',
-                 renderer='kotti_dashboard:templates/custom-content-alternative.pt')
-    def alternative_view(self):
-        """ Alternative view for :class:`kotti_dashboard.resources.CustomContent`.
-        This view requires the JS / CSS resources defined in
-        :mod:`kotti_dashboard.fanstatic`.
-
-        :result: Dictionary needed to render the template.
-        :rtype: dict
-        """
-
-        css_and_js.need()
-
-        return {
-            'foo': _(u'bar'),
-        }
